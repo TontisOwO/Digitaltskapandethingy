4e83f464393032ed5bd20a6f35ccb48afc32be30
Azure Biander <azure.biander@learnet.se>
2024-01-30 09:05:42 +0100

    Mouse Over Script
    player as example

--- Assets/Scripts/Basic_Touch_Script.cs
+++ Assets/Scripts/Basic_Touch_Script.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Basic_Touch_Script : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public TextMeshProUGUI text;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnMouseOver()
     {
         
     }
