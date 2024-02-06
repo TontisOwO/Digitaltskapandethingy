f0b1c7e8449a4e5ddd0db982250847e60ffceeba
Adonai <adonai.osterberg@elev.ga.lbs.se>
2024-02-06 11:01:40 +0100

    Began Inventory Script

---
+++ Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    public SavedData myData = null;
+    public int ItemCount = 0;
+    public int MaxItems = 4;
+    public int MinItems = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DropItems()
+    {
+        
+    }
+}
