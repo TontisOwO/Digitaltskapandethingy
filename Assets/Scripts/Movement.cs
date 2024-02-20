066e07524fcfee7880e23dfe21126256c27d5695
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2024-02-20 11:05:39 +0100

    some comments

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,12 +10,16 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //Get the Position of the mouse
             Vector3 mousePos = Input.mousePosition;
             Debug.Log("The current x position is " + mousePos.x);
             Debug.Log("The current y position is " + mousePos.y);
 
+            //Scale the mouse correctly so it matches the camera
             mousePos.x = (mousePos.x / 34.25f) - 8.8f;
             mousePos.y = (mousePos.y / 379.5f) - 2.45f;
+
+            //Set the position of the character to scaled mouse and adjust for camera position
             gameObject.transform.position = mousePos + CameraScript.CameraPos;
         }
     }
