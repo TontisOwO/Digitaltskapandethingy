6673739000c56b59e976d77dbc3855265403635f
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2024-03-08 09:22:37 +0100

    Level 1 started sort of

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour
+public class CameraScript : MonoBehaviour   
 {
     public Vector3 cameraPos;
     public float minPos = -5;
