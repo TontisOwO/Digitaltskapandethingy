e517ee944e73676104bbd1d313fed69cb380b350
Adonai <adonai.osterberg@elev.ga.lbs.se>
2024-04-30 09:15:39 +0200

    Audio stuff

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public SavedData myData;
+
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("------- Audio Clip -------")]
+    public AudioClip background;
+    public AudioClip click;
+    public AudioClip planks;
+    public AudioClip nails;
+    public AudioClip titlemusic;
+    public AudioClip zip;
+    public AudioClip unzip;
+
+    private void Start()
+    {
+        if (SceneManager.loadedSceneCount == 0)
+        {
+            musicSource.clip = titlemusic;
+            musicSource.Play();
+        }
+        else
+        {
+            musicSource.clip = background;
+            musicSource.Play();
+        }
+    }
+
+    public void Update()
+    {
+        musicSource.volume = myData.SFXVolume / 100;
+        SFXSource.volume = myData.SFXVolume / 100;
+
+        if ( Input.GetMouseButtonDown(0))
+        {
+            SFXSource.clip = click;
+            SFXSource.Play();
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
