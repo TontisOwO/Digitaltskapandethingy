b11670f8719d0d4387eeadd52a8a1d126963a131
Adonai <adonai.osterberg@elev.ga.lbs.se>
2024-05-17 08:42:54 +0200

    toilet done dfsaåmk ewomlgznmjeöskrfnzbjnerhi

--- Assets/Scripts/GlassMovement.cs
+++ Assets/Scripts/GlassMovement.cs
@@ -9,6 +9,10 @@ using UnityEngine.UIElements;
 public class GlassMovement : MonoBehaviour
 {
     public Vector3 mousePos;
+    public GameObject left;
+    public GameObject right;
+    public GameObject top;
+    public GameObject bottom;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +26,24 @@ public class GlassMovement : MonoBehaviour
         if (transform.position.z != 1)
         {
 
-        }    
+        }   
+        
+        if (transform.position.x >= right.transform.position.x)
+        {
+            transform.position = right.transform.position;
+        }
+        if (transform.position.x <= left.transform.position.x)
+        {
+            transform.position = left.transform.position;
+        }
+        if (transform.position.y >= top.transform.position.y)
+        {
+            transform.position = top.transform.position;
+        }
+        if (transform.position.y <= bottom.transform.position.y)
+        {
+            transform.position = bottom.transform.position;
+        }
     }
 
     void OnMouseDrag()
