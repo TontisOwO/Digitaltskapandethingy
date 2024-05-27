59c677beec453eb54f2e80c2a8cfb32cefdb37cd
Azure Biander <azure.biander@learnet.se>
2024-05-27 13:56:59 +0200

    Floor 3 Puzzle is done

--- Assets/Scripts/GameScripts/CloseButton.cs
+++ Assets/Scripts/GameScripts/CloseButton.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class CloseButton : MonoBehaviour
 {
     public SceneLoader sceneLoader;
+    public SavedData saveData;
     public string scene;
     public bool Specialbutton;
     public bool Startbutton;
     public bool NewGame;
     public bool enterpuzzle;
+    public bool floor3Door;
     private void OnMouseDown()
     {
-        if (!Specialbutton)
+        if (!floor3Door || saveData.floor3PuzzleDone)
         {
-            sceneLoader.ExitPuzzleScene(scene);
-        }
-        else if (Startbutton)
-        {
-            sceneLoader.ContinueGame();
-        }
-        else if (NewGame)
-        {
-            sceneLoader.EraseData();
-        }
-        else if (enterpuzzle)
-        {
-            sceneLoader.EnterPuzzleScene(scene);
-        }
-        else
-        {
-            sceneLoader.LoadScene(scene);
+            if (!Specialbutton)
+            {
+                sceneLoader.ExitPuzzleScene(scene);
+            }
+            else if (Startbutton)
+            {
+                sceneLoader.ContinueGame();
+            }
+            else if (NewGame)
+            {
+                sceneLoader.EraseData();
+            }
+            else if (enterpuzzle)
+            {
+                sceneLoader.EnterPuzzleScene(scene);
+            }
+            else
+            {
+                sceneLoader.LoadScene(scene);
+            }
         }
     }
 }
